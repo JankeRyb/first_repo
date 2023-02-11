bbd442d2205caa984dfbd781fbe4a86cf06f691f
JankeRyb <j7rybicki@gmail.com>
2023-02-11 14:51:27 +0100

    11.02.2023_final

--- selenium_2.py
+++ selenium_2.py
@@ -6,8 +6,21 @@ import time
 driver=webdriver.Chrome()
 driver.get("http://www.saucedemo.com/")
 print("Nazwa strony:",driver.title)
+username_box=driver.find_element('id','user-name')
+password_box=driver.find_element('id','password')
+login_box=driver.find_element('id','login-button')
 
-time.sleep(3)
+username_box.click()
+username_box.send_keys('standard_user')
+
+password_box.click()
+password_box.send_keys('secret_sauce')
+
+login_box.click()
+
+driver.get_screenshot_as_file('screenshot.png')
+
+time.sleep(60)
 
 
 # button_1_accept= driver.find_element('id',"L2AGLb")
