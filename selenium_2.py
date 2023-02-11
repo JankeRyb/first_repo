278621361304046f4e502fe2d8407cc540f9f233
JankeRyb <j7rybicki@gmail.com>
2023-02-11 15:02:36 +0100

    11.02.2023_final

--- selenium_2.py
+++ selenium_2.py
@@ -1,24 +1,37 @@
 from selenium import webdriver
 import time
+import datetime
 #from selenium.webdriver.common.by import By
 
+teraz=datetime.datetime.now()
+# print(teraz)
+# print(type(teraz))
+screenshot='screenshot'+teraz.strftime('%H:%M:%S')+'.png'
+print(screenshot)
 
 driver=webdriver.Chrome()
 driver.get("http://www.saucedemo.com/")
 print("Nazwa strony:",driver.title)
-username_box=driver.find_element('id','user-name')
+
+try:
+    username_box=driver.find_element('id','user-namesdsd')
+    username_box.click()
+    username_box.send_keys('standard_user')
+except:
+    driver.get_screenshot_as_file(screenshot)
+
+
 password_box=driver.find_element('id','password')
 login_box=driver.find_element('id','login-button')
 
-username_box.click()
-username_box.send_keys('standard_user')
+
 
 password_box.click()
 password_box.send_keys('secret_sauce')
 
 login_box.click()
 
-driver.get_screenshot_as_file('screenshot.png')
+driver.get_screenshot_as_file(screenshot)
 
 time.sleep(60)
 
